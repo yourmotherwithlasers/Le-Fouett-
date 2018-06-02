200de31c7c0d931053c62cf5d38f214811faf7fb
yourmotherwithlasers <39866226+yourmotherwithlasers@users.noreply.github.com>
2018-06-02 19:09:19 -0300

    Add files via upload

---
+++ fouette.ino
@@ -0,0 +1,46 @@
+//inclui Servo Libery
+#include <Servo.h>
+//declara pino do servo
+int servoPin = 5;
+const int minSec = 795;
+const int maxSec = 2180;
+//Cria objeto servo
+Servo Servo1;
+// define pinos driver
+const int stepPin = 3; 
+const int dirPin = 4; 
+ 
+void setup() {
+  // Sets the two pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  //Define pino do servo como sinal
+  Servo1.attach(servoPin, minSec, maxSec);
+}
+void loop() {
+  digitalWrite(dirPin,HIGH); //muda direcao do stepper
+  // 250 revolucoes ( curso total)
+  for(int x = 0; x < 250; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(5000); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(5000); 
+  }
+  delay(1000); // uma pausa
+  //faz servo ir para zero graus
+  Servo1.write(1);
+  delay(1000); // outra pausa
+  
+  digitalWrite(dirPin,LOW); //muda direcao stepper
+  // 250 revolucoes curso
+  for(int x = 0; x < 250; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(5000);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(5000);
+  }
+  delay(1000); // outra pausa
+  //Faz servo ir para 180 graus
+  Servo1.write(180);
+  delay(1000);
+}
---
+++ fouetteavecservo.ino
@@ -0,0 +1,31 @@
+//inclui biblioteca servo
+#include Servo.h;
+//declara servos jr no pino 4
+int servoum = 4;
+int servodois = 7;
+const int minSec = 795;
+const int maxSec = 2180;
+//declara servo futaba no pino 7
+int servodois = 7;
+Servo Servo1;//cria primeiro servo
+Servo Servo2;//cria segundo servo
+
+
+void setup() {
+  Servo1.attach (servoum, minSec, maxSec);
+  Servo2.attach (servodois, minSec, maxSec);
+
+}
+
+void loop() {
+  Servo1.write(1);
+  delay(500);
+  Servo2.write(1);
+  delay(500);
+  Servo1.write(180);
+  delay(500);
+  Servo2.write(180);
+  delay(500);
+  // verificar delays
+
+}
